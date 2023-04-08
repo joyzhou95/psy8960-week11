349007ed3ff08dbd85fbb03ebbbae67ef773e36f
Joy Zhou <zhou1559@umn.edu>
2023-04-08 10:42:29 -0500

    Commented the cluster R script & cleaned up unnecessary codes

--- R/week11-cluster.R
+++ R/week11-cluster.R
@@ -23,13 +23,14 @@ gss_tbl <- select(gss_tbl_original,
 shuffle_row <- sample(nrow(gss_tbl))
 shuffled_gss <- gss_tbl[shuffle_row,]
 split <- round(nrow(shuffled_gss)*0.75)
-train <- shuffled_gss[1:split,]
+train1 <- shuffled_gss[1:split,]
 test <- shuffled_gss[(split + 1):nrow(shuffled_gss),]
-folds <- createFolds(train$workhours, k = 10)
+folds <- createFolds(train1$workhours, k = 10)
 
-train <- sapply(train, as.numeric)
+train <- sapply(train1, as.numeric)
 
 ## OLS regression model
+### Add the functions tic and toc to all models to record the time for running the models
 tic()
 ols_model <- train(
   workhours ~.,
@@ -45,6 +46,7 @@ ols_model <- train(
     indexOut = folds
   )
 )
+### Assign the running time to an object to present the numbers in table 4 dynamically
 ols_runtime <- toc()
 
 ols_test <- predict(ols_model, test, na.action = na.pass)
@@ -116,37 +118,12 @@ gbm_test_r2 <-cor(gbm_test , test$workhours)^2
 
 ##########Parallelization##########
 ## Make clusters for parallelization and assign the cluster to all following functions that are parallelized 
-cluster <- makeCluster(7)
+## Doubled the number of clusters created when running on my laptop   
+cluster <- makeCluster(15)
 registerDoParallel(cluster)
 
-# Data Import and Cleaning
-gss_tbl_original <- read_sav("../data/GSS2016.sav") %>%
-  filter(!is.na(MOSTHRS)) %>%
-  ## Remove the other two workhour variables by not selecting them from the read datafile 
-  select(-HRS1, -HRS2)
-
-
-gss_tbl <- select(gss_tbl_original,
-                  which(colSums(is.na(gss_tbl_original))/nrow(gss_tbl_original) < 0.75)) %>%
-  rename(workhours = MOSTHRS)
-
-# Visualization
-gss_tbl %>%
-  ggplot(aes(workhours)) +
-  geom_histogram() +
-  labs(x = "Number of Hourse Worked Last Week", y = "Frequency")
-
-# Analysis
-shuffle_row <- sample(nrow(gss_tbl))
-shuffled_gss <- gss_tbl[shuffle_row,]
-split <- round(nrow(shuffled_gss)*0.75)
-train <- shuffled_gss[1:split,]
-test <- shuffled_gss[(split + 1):nrow(shuffled_gss),]
-folds <- createFolds(train$workhours, k = 10)
-
-train <- sapply(train, as.numeric)
-
 ## OLS regression model
+### Add the functions tic and toc to all models to record the time for running the models
 tic()
 ols_model <- train(
   workhours ~.,
@@ -162,10 +139,8 @@ ols_model <- train(
     indexOut = folds
   )
 )
+### Assign the running time to an object to present the numbers in table 4 dynamically
 ols_runtime_p <- toc()
-
-ols_test <- predict(ols_model, test, na.action = na.pass)
-ols_test_r2 <- cor(ols_test, test$workhours)^2
 
 ## Elastic net model 
 tic()
@@ -185,9 +160,6 @@ glmnet_model <- train(
 )
 glm_runtime_p <-toc()
 
-glmnet_test <- predict(glmnet_model, test, na.action = na.pass)
-glmnet_test_r2 <-cor(glmnet_test, test$workhours)^2
-
 ## Random forest model
 tic()
 rf_model <- train(
@@ -206,9 +178,6 @@ rf_model <- train(
 )
 rf_runtime_p <-toc()
 
-rf_test <- predict(rf_model, test, na.action = na.pass)
-rf_test_r2 <-cor(rf_test, test$workhours)^2
-
 ## eXtreme Gradient Boosting
 tic()
 gbm_model <- train(
@@ -226,9 +195,6 @@ gbm_model <- train(
   )
 )
 gbm_runtime_p <- toc()
-
-gbm_test <- predict(gbm_model, test, na.action = na.pass)
-gbm_test_r2 <-cor(gbm_test , test$workhours)^2
 
 # Stop parallel processing
 stopCluster(cluster)
@@ -250,14 +216,25 @@ ho_rsq2 <- sapply(ho_rsq1, formatC, format = "f", digits = 2)
 ho_rsq <- str_remove(ho_rsq2, pattern = "^0")
 
 table3_tbl <- tibble(algo, cv_rsq, ho_rsq) %>%
+  ## Save table 3 as a csv file
   write_csv("table3.csv")
 
-##
-original <- c(round(ols_runtime$toc-ols_runtime$tic,2), round(glm_runtime$toc-glm_runtime$tic,2),
+## Create the object with the time of running the models in the original way using supercomputers and format them to two decimal places
+supercomputer <- c(round(ols_runtime$toc-ols_runtime$tic,2), round(glm_runtime$toc-glm_runtime$tic,2),
               round(rf_runtime$toc-rf_runtime$tic,2), round(gbm_runtime$toc-gbm_runtime$tic,2))
-parallelized <- c(round(ols_runtime_p$toc-ols_runtime_p$tic,2), round(glm_runtime_p$toc-glm_runtime_p$tic,2),
+
+## Create the object with the time of running the models in the parallelized way using supercomputers and format them to two decimal places
+supercomputer_15 <- c(round(ols_runtime_p$toc-ols_runtime_p$tic,2), round(glm_runtime_p$toc-glm_runtime_p$tic,2),
                   round(rf_runtime_p$toc-rf_runtime_p$tic,2), round(gbm_runtime_p$toc-gbm_runtime_p$tic,2))
 
-table4_tbl <- tibble(algo, cv_rsq, ho_rsq, original, parallelized) %>% 
+## Create table 3 including the models names, r-square values, and running time created above
+table4_tbl <- tibble(algo, cv_rsq, ho_rsq, supercomputer, "supercomputer-15"=supercomputer_15) %>% 
+  ## Save table 4 as a csv file
   write_csv("table4.csv")
+
+## Questions
+### Which models benefited most from moving to the supercomputer and why?
+### What is the relationship between time and the number of cores used?
+### If your supervisor asked you to pick a model for use in a production model, would you recommend using the supercomputer and why? Consider all four tables when providing an answer.
+
 
--- R/week11-local.R
+++ R/week11-local.R
@@ -37,7 +37,6 @@ folds <- createFolds(train1$workhours, k = 10)
 train <- sapply(train1, as.numeric)
 
 ## OLS regression model
-
 ### Add the functions tic and toc to all models to record the time for running the models
 tic()
 ols_model <- train(
@@ -126,35 +125,9 @@ gbm_test_r2 <-cor(gbm_test , test$workhours)^2
 
 ##########Parallelization##########
 ## Make clusters for parallelization and assign the cluster to all following functions that are parallelized 
+## Set the clusters to 7 given that my laptop has 8 cores 
 cluster <- makeCluster(7)
 registerDoParallel(cluster)
-
-# Data Import and Cleaning
-gss_tbl_original <- read_sav("../data/GSS2016.sav") %>%
-  filter(!is.na(MOSTHRS)) %>%
-  ## Remove the other two workhour variables by not selecting them from the read datafile 
-  select(-HRS1, -HRS2)
-
-
-gss_tbl <- select(gss_tbl_original,
-                  which(colSums(is.na(gss_tbl_original))/nrow(gss_tbl_original) < 0.75)) %>%
-  rename(workhours = MOSTHRS)
-
-# Visualization
-gss_tbl %>%
-  ggplot(aes(workhours)) +
-  geom_histogram() +
-  labs(x = "Number of Hourse Worked Last Week", y = "Frequency")
-
-# Analysis
-shuffle_row <- sample(nrow(gss_tbl))
-shuffled_gss <- gss_tbl[shuffle_row,]
-split <- round(nrow(shuffled_gss)*0.75)
-train <- shuffled_gss[1:split,]
-test <- shuffled_gss[(split + 1):nrow(shuffled_gss),]
-folds <- createFolds(train$workhours, k = 10)
-
-train <- sapply(train, as.numeric)
 
 ## OLS regression model
 ### Add the functions tic and toc to all models to record the time for running the models
@@ -176,9 +149,6 @@ ols_model <- train(
 ### Assign the running time to an object to present the numbers in table 2 dynamically
 ols_runtime_p <- toc()
 
-ols_test <- predict(ols_model, test, na.action = na.pass)
-ols_test_r2 <- cor(ols_test, test$workhours)^2
-
 ## Elastic net model 
 tic()
 glmnet_model <- train(
@@ -196,9 +166,6 @@ glmnet_model <- train(
   )
 )
 glm_runtime_p <-toc()
-
-glmnet_test <- predict(glmnet_model, test, na.action = na.pass)
-glmnet_test_r2 <-cor(glmnet_test, test$workhours)^2
 
 ## Random forest model
 tic()
@@ -218,9 +185,6 @@ rf_model <- train(
 )
 rf_runtime_p <-toc()
 
-rf_test <- predict(rf_model, test, na.action = na.pass)
-rf_test_r2 <-cor(rf_test, test$workhours)^2
-
 ## eXtreme Gradient Boosting
 tic()
 gbm_model <- train(
@@ -238,9 +202,6 @@ gbm_model <- train(
   )
 )
 gbm_runtime_p <- toc()
-
-gbm_test <- predict(gbm_model, test, na.action = na.pass)
-gbm_test_r2 <-cor(gbm_test , test$workhours)^2
 
 # Stop parallel processing
 stopCluster(cluster)
@@ -263,14 +224,25 @@ ho_rsq <- str_remove(ho_rsq2, pattern = "^0")
 
 table1_tbl <- tibble(algo, cv_rsq, ho_rsq) 
 
-## Create the object with the time of running the models in the original way and format them to keep only two decimal places
+## Create the object with the time of running the models in the original way and format them to two decimal places
 original <- c(round(ols_runtime$toc-ols_runtime$tic,2), round(glm_runtime$toc-glm_runtime$tic,2),
               round(rf_runtime$toc-rf_runtime$tic,2), round(gbm_runtime$toc-gbm_runtime$tic,2))
 
-## Create the object with the time of running the models in the parallelized way and format them to keep only two decimal places
+## Create the object with the time of running the models in the parallelized way and format them to two decimal places
 parallelized <- c(round(ols_runtime_p$toc-ols_runtime_p$tic,2), round(glm_runtime_p$toc-glm_runtime_p$tic,2),
                   round(rf_runtime_p$toc-rf_runtime_p$tic,2), round(gbm_runtime_p$toc-gbm_runtime_p$tic,2))
 
 ## Create table 2 including the models names, r-square values, and running time created above
 table2_tbl <- tibble(algo, cv_rsq, ho_rsq, original, parallelized)
+
+## Questions 
+#### Which models benefited most from parallelization and why?
+
+
+#### How big was the difference between the fastest and slowest parallelized model? Why?
+#### If your supervisor asked you to pick a model for use in a production model, which would you recommend and why? Consider both Table 1 and Table 2 when providing an answer.
+
+
+
+
 
